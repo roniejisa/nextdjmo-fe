83994f956ccc920c850a5794f4ab195e1c074622
roniejisa <roniejisa@gmail.com>
2025-02-18 14:41:22 +0700

    update code

--- src/app/(client)/(web)/(bqp)/kham-pha/page.jsx
+++ src/app/(client)/(web)/(bqp)/kham-pha/page.jsx
@@ -3,8 +3,8 @@ import ExplodeClient from "./ExplodeClient";
 const Explode = () => {
   return (
     <div className="lg:px-10">
-      <h1 className="text-center text-2xl mb-10 relative before:h-[2px] before:absolute before:w-full before:bg-white before:left-0 before:top-1/2 before:translate-y-1/2">
-        <span className="relative z-10 bg-[#221f20]">Khám phá</span>
+      <h1 className="text-center text-4xl mb-10 relative before:h-[2px] before:absolute before:w-2/3 before:bg-white before:left-1/2 before:-translate-x-1/2 before:top-1/2 before:translate-y-1/2">
+        <span className="relative z-10 bg-[#221f20] px-4">Khám phá</span>
       </h1>
       <ExplodeClient />
     </div>
--- src/context/client/ClientProvider.jsx
+++ src/context/client/ClientProvider.jsx
@@ -1,57 +1,22 @@
 "use client";
-import { getDataDraftOrder } from "@/app/(client)/(web)/gio-hang/action";
 import ModalSeach from "@/components/ui/client/ModalSeach";
 import { createContext, useEffect, useRef, useState } from "react";
 
 export const ClientContext = createContext();
 const ClientProvider = ({ children }) => {
   const [showModalSearch, setShowModalSearch] = useState(false);
-  const [updateCart, setUpdateCart] = useState(false);
-  const [totalOrders, setTotalOrders] = useState(0);
-  const [orders, setOrders] = useState([]);
 
   // HEADER
   const headerRef = useRef();
   const sectionRef = useRef({
-    home: []
+    home: [],
   });
-
-  const fetchData = async () => {
-    const reponse = await getDataDraftOrder();
-    if (reponse && reponse.status == 200 && reponse.data.length) {
-      setOrders(reponse.data);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    if (updateCart) {
-      fetchData();
-      setUpdateCart(false);
-    }
-  }, [updateCart]);
-
-  useEffect(() => {
-    const count = orders.reduce((acc, item) => {
-      return +acc + +item.qty;
-    }, 0);
-    setTotalOrders(count);
-  }, [orders]);
 
   return (
     <ClientContext.Provider
       value={{
         showModalSearch,
         setShowModalSearch,
-        totalOrders,
-        setTotalOrders,
-        orders,
-        setOrders,
-        updateCart,
-        setUpdateCart,
         headerRef,
         sectionRef,
       }}
